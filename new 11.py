f88d339e326ad7306652285abdce6ff43dd8bf6b
R Ganesh <68594076+rganesh203@users.noreply.github.com>
2024-09-01 19:33:55 +0530

    Add files via upload

---
+++ new 11.py
@@ -0,0 +1,10 @@
+In computer science, a data structure is a logical way of organizing data in computer memory so that it can be used effectively. A data structure allows data to be added, removed, stored and maintained in a structured manner. Python supports two types of data structures:
+
+Non-primitive data types: Python has list, set, and dictionary as its non-primitive data types which can also be considered its in-built data structures.
+User-defined data structures: Data structures that aren’t supported by python but can be programmed to reflect the same functionality using concepts supported by python are user-defined data structures. There are many data structure that can be implemented this way:
+Linked list
+Stack
+Queue
+Tree
+Graph
+Hashmap
